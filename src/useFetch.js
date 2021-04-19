961ad35be11aa16d85bd6ca7de97c4363f16a21c
Tanvi P Ambre <Tanvi.Ambre@ibm.com>
2021-04-19 11:13:00 +0530

    Custom Hooks

---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react'
+
+const useFetch = (url) =>{
+
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(true);
+    
+    const [error, setError] = useState(null);
+
+
+    useEffect(()=>{
+        
+        //console.log('use effect ran');
+        fetch(url)
+          .then(res=>{
+              if(!res.ok){
+                  throw Error('Could not fetch the data')
+              }
+            return res.json()
+          })
+          .then((data)=>{
+            console.log(data);
+            setData(data);
+            setIsPending(false);
+            setError(null)
+          })
+          .catch(err=>{
+            console.log(err.message);
+            setIsPending(false);
+            setError(err.message)
+          })
+      },[url]);
+    return{data, isPending,error}
+}
+
+export default useFetch;
